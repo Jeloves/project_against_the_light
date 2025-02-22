98dd3c9a708df37be228f60fdebda614f6778685
Anjelo Valera <anjelovaleraecube@icloud.com>
2025-02-22 17:08:47 -0500

    Created TimeControl ui

---
+++ constants.ts
@@ -0,0 +1,20 @@
+// Resource Names
+export const title_resource_0 = "supply"
+export const title_resource_0p = "supplies"
+export const title_resource_1 = "intel"
+export const title_resource_1p = "intel"
+export const title_resource_2 = "food"
+export const title_resource_2p = "food"
+export const title_resource_3 = "researcher"
+export const title_resource_3p = "researchers"
+export const title_resource_4 = "engineer"
+export const title_resource_4p = "engineers"
+export const title_resource_5 = "population"
+export const title_resource_5p = "population"
+
+export const url_resource_0 = "/icons/supply.svg";
+export const url_resource_1 = "/icons/antenna.svg";
+export const url_resource_2 = "/icons/apple.svg";
+export const url_resource_3 = "/icons/beaker.svg";
+export const url_resource_4 = "/icons/tools.svg";
+export const url_resource_5 = "/icons/people.svg";
