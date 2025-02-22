36f96bb3f725b6075ec802f22cc8da919593a9ec
Anjelo Valera <anjelovaleraecube@icloud.com>
2025-02-22 14:13:24 -0500

    Created basic structure of page

--- pages/world-map.tsx
+++ pages/world-map.tsx
@@ -1,9 +1,21 @@
+import styles from "@/styles/NewPage.module.css";
+
 const WorldMap = () => {
     return (
-        <div>
-            <h1>New Page</h1>
-            <p>Welcome to the new page!</p>
-        </div>
+        <main>
+            <div className={styles.topbar}>
+                <div className={styles.resources}>Resources</div>
+                <div className={styles.time}>Date/Time</div>
+            </div>
+            <div className={styles.midbar}>
+                <div className={styles.leftmenu}>MENU</div>
+                <div className={styles.rightmenu}>Menu</div>
+            </div>
+            <div className={styles.botbar}>
+                <div className={styles.log}>Action Log</div>
+                <div className={styles.missions}>Missions</div>
+            </div>
+        </main>
     );
 };
 
