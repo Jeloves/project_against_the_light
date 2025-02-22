8f7706b05805ba1293a132e6708740a7c3d06797
Anjelo Valera <anjelovaleraecube@icloud.com>
2025-02-22 14:05:28 -0500

    created WorldMap page

---
+++ pages/world-map.tsx
@@ -0,0 +1,10 @@
+const WorldMap = () => {
+    return (
+        <div>
+            <h1>New Page</h1>
+            <p>Welcome to the new page!</p>
+        </div>
+    );
+};
+
+export default WorldMap;
