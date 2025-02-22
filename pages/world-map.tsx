41600d73a1b65d2b2b6645c237595ce835c12676
Anjelo Valera <anjelovaleraecube@icloud.com>
2025-02-22 16:30:46 -0500

    Created _app page to manipulate higher elements

--- pages/world-map.tsx
+++ pages/world-map.tsx
@@ -1,6 +1,6 @@
 import styles from "@/styles/world-map.module.scss";
 import ResourceMeter from "@/components/world-map/ResourceMeter";
-import { title_resource_0 } from "@/constants";
+import { title_resource_0, title_resource_1, title_resource_2, title_resource_3, title_resource_4, title_resource_5 } from "@/constants";
 
 const WorldMap = () => {
     return (
@@ -8,6 +8,11 @@ const WorldMap = () => {
             <div className={styles.topbar}>
                 <div className={styles.resources}>
                     <ResourceMeter resourceType={title_resource_0}/>
+                    <ResourceMeter resourceType={title_resource_1}/>
+                    <ResourceMeter resourceType={title_resource_2}/>
+                    <ResourceMeter resourceType={title_resource_3}/>
+                    <ResourceMeter resourceType={title_resource_4}/>
+                    <ResourceMeter resourceType={title_resource_5}/>
                 </div>
                 <div className={styles.time}>Date/Time</div>
             </div>
