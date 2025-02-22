e88af0210ae01ac1f8a47a5aecb31516c50e4b43
Anjelo Valera <anjelovaleraecube@icloud.com>
2025-02-22 14:03:56 -0500

    Installed vercel and sassCSS

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Layout from "../components/Layout";
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
+    <li><Link href="/world-map">Navigate to World Map</Link></li>
     <p>
       <Link href="/about">About</Link>
     </p>
